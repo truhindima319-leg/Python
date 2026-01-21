add9757733341c77e276896df1d353e3298e96ee
truhindima319-leg <truhindima319@gmail.com>
2026-01-21 19:32:21 +0500

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task_2.py
@@ -0,0 +1,8 @@
+# TODO решите задачу
+Импорт JSON
+def task() -> float:
+    с open('input.json', 'r', encoding='utf-8') в качестве файла:
+        data = json. load(файл))
+    total = sum(product['score'] * item ['weight'] для item in data)
+     возвратный раунд (всего, 3)
+print(задача)())
